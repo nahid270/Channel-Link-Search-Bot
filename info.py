a0bce1c34f0695989424ab0e4ab747a922f35ca6
nahid270 <110238003+nahid270@users.noreply.github.com>
2025-09-22 15:38:53 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
-API_ID       = 28870226
-API_HASH     = "a5b1ff3f75941649bf5bc159782f0f00"
+API_ID       = 26195153
+API_HASH     = "cffc45876502fd70a6d20141b3bd1c8f"
 BOT_TOKEN    = "7956858190:AAHjy4dccxZxiijp9GM87SdsW95gGtk7Qk4"
 SESSION      = ""
-DATABASE_URI = "mongodb+srv://polay60829:polay60829@cluster0.2qxpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = "mongodb+srv://galop32430:galop32430@cluster0.lslkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1003044100338
 ADMIN        = 7528643689

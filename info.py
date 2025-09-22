8d068f3fe3023f87d0305f17124d988da9aa88c0
nahid270 <110238003+nahid270@users.noreply.github.com>
2025-09-22 15:40:02 +0600

    Update info.py

--- info.py
+++ info.py
@@ -3,5 +3,5 @@ API_HASH     = "cffc45876502fd70a6d20141b3bd1c8f"
 BOT_TOKEN    = "7956858190:AAHjy4dccxZxiijp9GM87SdsW95gGtk7Qk4"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://galop32430:galop32430@cluster0.lslkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1003044100338
-ADMIN        = 7528643689
+LOG_CHANNEL  = -1002905500231
+ADMIN        = 7409347279
